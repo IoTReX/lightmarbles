1f6f4d4b95c1c7d612b450ad42cf1b55cbbb0958
samjulien <spjulien@gmail.com>
2016-04-30 12:58:33 -0700

    refine build process and clean up app

--- client/js/app.js
+++ client/js/app.js
@@ -1,22 +1,14 @@
 var socket = io.connect('http://localhost:8080', {reconnect: true});
 
-function setupMarbelous(Rx){
-  var recordObservation = Marbelous.createMarbleDisplay(document.getElementById('marbles-container'));
+const recordObservation = Marbelous.createMarbleDisplay(document.getElementById('marbles-container'));
 
-  Rx.Observable.prototype.visualize = function(name){
-    this.inspectTime(60).subscribe(function(e){
-    	return recordObservation(name,e);
-    });
-
-    return this;
-  }
+function visualize(name,observable) {
+  observable.subscribe( e => recordObservation(name,e) );
 }
 
-setupMarbelous(Rx);
+var source$ = Rx.Observable.fromEvent(socket, 'photoData').distinctUntilChanged();
 
-source$ = Rx.Observable.fromEvent(socket, 'photoData').distinctUntilChanged();
-
-source$.visualize('Photosensor Data');
+visualize('Photosensor Data', source$);
 
 var mins = source$
   .scan(Math.min)
@@ -26,14 +18,12 @@ var maxes = source$
   .scan(Math.max)
   .distinctUntilChanged();
 
-mins.visualize('Minimum Reading');
-maxes.visualize('Maximum Reading');
+visualize('Minimum Reading', mins);
+visualize('Maximum Reading', maxes);
 
 var sub = source$
-.subscribe(function(x){
+.subscribe(function (x) {
 		console.log('light sensor value: ' + x);
-        //sin.set({freq:x});
-        
         var sine1 = T("sin", {freq:x*.25, mul:0.5});
         var sine2 = T("sin", {freq:x, mul:0.5});
 
@@ -47,51 +37,18 @@ var sub = source$
 		console.info('done');
 	});
 
-// var VCO = T("sin").play();
-// var VCF = T("hpf", {cutoff:1600, Q:10}, VCO).play();
-
-// var sub2 = source$
-// .subscribe(function(x){
-//     console.log('light sensor value: ' + x);
-//         VCO.set({freq:x*.5});
-//   }, function(err){
-//     console.error(err);
-//   }, function(){
-//     console.info('done');
-//   });
-
-source2$ = Rx.Observable.fromEvent(socket, 'photoData2').distinctUntilChanged();
-source2$.visualize('Photosensor 2 Data');
-
-// var cutoff = T("sin", {freq:"400ms", mul:300, add:1760}).kr();
-// var VCO2 = T("saw", {mul:0.2});
-// var VCF2 = T("lpf", {cutoff:cutoff, Q:20}, VCO2).play();
+var source2$ = Rx.Observable.fromEvent(socket, 'photoData2').distinctUntilChanged();
+visualize('Photosensor 2 Data', source2$);
 
 var sine3 = T("sin", {mul:0.5}).play();
 
 var sub3 = source2$
 .subscribe(function(x){
     console.log('light sensor 2 value: ' + x);
-        //VCF.set({cutoff:x*.25})
-
-        // VCO2.freq.value   = x*.4;
-        // VCF2.set({cutoff:x*.5});
-
         var calc = 880-(x/4);
         sine3.set({freq:x});
-        //var sine4 = T("sin", {freq:calc, mul:0.5});
-
-        
-
-        // T("perc", {r:300}, sine3, sine4).on("ended", function() {
-        //   this.pause();
-        // }).bang().play();
   }, function(err){
     console.error(err);
   }, function(){
     console.info('done');
   });
-// setTimeout(function(){
-// 	console.log('disposing');
-// 	sub.dispose();
-// }, 5000);
--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ var plug = require('gulp-load-plugins')();
 
 var del = require('del');
 var runSequence = require('run-sequence');
-var _ = require('lodash');
 var fs = require('fs');
 
 var env = plug.util.env;
@@ -16,43 +15,29 @@ var log = plug.util.log;
 var inject = require("gulp-inject");
 
 gulp.task('build', false, ['clean'], function (cb) {
-    log('Notification set to: ' + notifyOn);
-    runSequence(['css'], ['js', 'rev-and-inject', 'buildNotify'], cb);
+    runSequence(['css'], ['js', 'inject', 'buildNotify'], cb);
 });
 
 gulp.task('build:noclean', false, [], function (cb) {
-    log('Notification set to: ' + notifyOn);
-    runSequence(['css'], ['js', 'rev-and-inject', 'buildNotify'], cb);
+    runSequence(['css'], ['js', 'inject', 'buildNotify'], cb);
 });
 
-gulp.task('buildNotify', false, ['rev-and-inject'], function () {
-    if (notifyOn !== 'silent') {
-        return gulp.src('').pipe(plug.notify({
+gulp.task('buildNotify', false, ['inject'], function () {
+    return gulp.src('').pipe(plug.notify({
             onLast: true,
             message: 'Code compiled and copied to output folder!'
         }));
-    }
-    else {
-        return true;
-    }
 });
 
 gulp.task('clean', false, function () {
-    var env = isDebug ? pkg.paths.tmpFolder : pkg.paths.distFolder;
-    var paths = [env + 'index.html', env + 'assets/*'];
+    var root = pkg.paths.dist;
+    var paths = [root + '/index.html', root + '/js/*', root + '/css/*'];
     log('Cleaning: ' + plug.util.colors.red(paths));
     return del.sync(paths, { force: true }, function (err) {
         if (err) {
             log(err);
         }
     });
-});
-
-gulp.task('analyze', 'Lints the JavaScript to report errors and warnings.', function () {
-    log('Analyzing source with JSHint and JSCS');
-    var jshint = analyzejshint([].concat(pkg.paths.js, './.jshintrc');
-    var jscs = analyzejscs([].concat(pkg.paths.js), './.jscsrc');
-    return merge(jshintTests, jshint /*, jscs*/);
 });
 
 gulp.task('js', false, [], function () {
@@ -73,7 +58,7 @@ gulp.task('vendorjs', false, function () {
     var source = [].concat(pkg.paths.vendorjs);
     return gulp.src(source)
         .pipe(plug.concat('vendor.min.js'))
-        .pipe(plug.uglify())
+        //.pipe(plug.uglify()) //Uncomment if you want to squash your vendor files, but it can be a hassle during dev.
         .pipe(gulp.dest(pkg.paths.dist + '/js'));
 });
 
@@ -93,47 +78,30 @@ gulp.task('vendorcss', false, [], function () {
         .pipe(gulp.dest(pkg.paths.dist + '/css'));
 });
 
-gulp.task('rev-and-inject', false, ['js', 'vendorjs', 'css', 'vendorcss'], function (cb) {
-    log('Rev\'ing files and injecting into index.html');
+gulp.task('inject', false, ['js', 'vendorjs', 'css', 'vendorcss'], function (cb) {
+    log('Injecting into index.html');
     var root = pkg.paths.dist;
-    var minified = root + '**/*.min.*';
+    var minified = root + '/**/*.min.*';
     var index = pkg.paths.client + '/index.html';
-    var minFilter = plug.filter(['**/*.min.*', '!**/*.map']);
     var indexFilter = plug.filter(['index.html']);
     
-
     return gulp
-        // Write the revisioned files
-        .src([].concat(minified, index)) // add all staged min files and index.html
-        .pipe(minFilter) // filter the stream to minified css and js
-        .pipe(plug.rev()) // create files with rev's
-        .pipe(gulp.dest(root)) // write the rev files
-        .pipe(minFilter.restore()) // remove filter, back to original stream
-
-        // inject the files into index.html
-        .pipe(indexFilter) // filter to index.html
-        .pipe(inject('/css/vendor.min.css', 'inject-vendor'))
+        .src([].concat(minified, index))
+        .pipe(indexFilter)
+        .pipe(inject('/css/vendor.min.css', 'vendor'))
         .pipe(inject('/css/all.min.css'))
-        .pipe(inject('/js/vendor.min.js', 'inject-vendor'))
+        .pipe(inject('/js/vendor.min.js', 'vendor'))
         .pipe(inject('/js/all.min.js'))
-        .pipe(gulp.dest(root)) // write the rev files
-        .pipe(indexFilter.restore()) // remove filter, back to original stream
-
-        // replace the files referenced in index.html with the rev'd files
-        .pipe(plug.revReplace())         // Substitute in new filenames
-        .pipe(gulp.dest(root)) // write the index.html file changes
-        .pipe(plug.rev.manifest()) // create the manifest (must happen last or we screw up the injection)
-        .pipe(gulp.dest(root)); // write the manifest
+        .pipe(gulp.dest(root))
 
     function inject(path, name) {
-        log('rev path: ' + path);
-        log('rev name: ' + name);
+        log('path: ' + path);
+        log('name: ' + name);
         var glob = root + path;
-        var env = isDebug ? 'tmp/' : 'dist/';
         var options = {
-            ignorePath: 'wwwroot/' + env,
+            ignorePath: '/dist',
             read: false,
-            addRootSlash: false
+            addRootSlash: true
         };
         if (name) { options.name = name; }
         return plug.inject(gulp.src(glob), options);
@@ -151,7 +119,7 @@ gulp.task('watch', false, [], function () {
 
     log('Watching JS files');
     gulp.watch('./client/**/*.js').on('change', logWatch);
-    gulp.watch('./client/**/*.js', ['build:noclean');
+    gulp.watch('./client/**/*.js', ['build:noclean']);
 
     function logWatch(event) {
         var eventMessage = 'File ' + event.path + ' was ' + event.type + ', running tasks...';
@@ -161,29 +129,3 @@ gulp.task('watch', false, [], function () {
         }));
     }
 });
-
-////////////////
-
-/**
- * Execute JSHint on given source files
- * @param  {Array} sources
- * @param  {string} jshintrc - file path string, allows custom rules per path
- * @return {Stream}
- */
-function analyzejshint(sources, jshintrc) {
-    return gulp
-        .src(sources)
-        .pipe(plug.jshint(jshintrc))
-        .pipe(plug.jshint.reporter('jshint-stylish'));
-}
-
-/**
- * Execute JSCS on given source files
- * @param  {Array} sources
- * @return {Stream}
- */
-function analyzejscs(sources) {
-    return gulp
-        .src(sources)
-        .pipe(plug.jscs('./.jscsrc'));
-}
